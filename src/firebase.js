85859734e7f888eac0ba74e8ef90fd66720a9858
Shreedutt <shreedutt77@gmail.com>
2022-05-10 19:02:30 +0530

    Update Firebase.js file
    Updated the configuration for app initialization in the file

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-	apiKey: "AIzaSyCskJgcGg6243wxjBU6Ojrw5cSg3B8s6D0",
-	authDomain: "work-flow-5c6c1.firebaseapp.com",
-	projectId: "work-flow-5c6c1",
-	storageBucket: "work-flow-5c6c1.appspot.com",
-	messagingSenderId: "196968450187",
-	appId: "1:196968450187:web:1f37a822eae2b1817eec8e",
+  apiKey: "AIzaSyBiIBvUxZoDbAcZ4neA2HHyXUDYUrqif8o",
+  authDomain: "mindmap-f5e95.firebaseapp.com",
+  projectId: "mindmap-f5e95",
+  storageBucket: "mindmap-f5e95.appspot.com",
+  messagingSenderId: "335186401350",
+  appId: "1:335186401350:web:e2d942657edc1fcb9f1247"
 });
 
 export const db = app.firestore();
