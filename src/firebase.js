0902d32b50901cdfe561dbed2b6efd3796ee8440
Shreedutt <shreedutt77@gmail.com>
2022-06-06 20:44:19 +0530

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBiIBvUxZoDbAcZ4neA2HHyXUDYUrqif8o",
-  authDomain: "mindmap-f5e95.firebaseapp.com",
-  projectId: "mindmap-f5e95",
-  storageBucket: "mindmap-f5e95.appspot.com",
-  messagingSenderId: "335186401350",
-  appId: "1:335186401350:web:e2d942657edc1fcb9f1247"
+  apiKey: "AIzaSyCITMhae04sLVImm08MuNE8XT7lSAkE1_A",
+  authDomain: "vizz-fae49.firebaseapp.com",
+  projectId: "vizz-fae49",
+  storageBucket: "vizz-fae49.appspot.com",
+  messagingSenderId: "874741298823",
+  appId: "1:874741298823:web:f0461b2e22ff5000df6a22"
 });
 
 export const db = app.firestore();
