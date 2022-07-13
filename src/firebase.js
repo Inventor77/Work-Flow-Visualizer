94be020dd61505d890005d2fc4e38f25d1719dbe
Shreedutt <shreedutt77@gmail.com>
2022-07-13 13:38:10 +0530

    updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCITMhae04sLVImm08MuNE8XT7lSAkE1_A",
-  authDomain: "vizz-fae49.firebaseapp.com",
-  projectId: "vizz-fae49",
-  storageBucket: "vizz-fae49.appspot.com",
-  messagingSenderId: "874741298823",
-  appId: "1:874741298823:web:f0461b2e22ff5000df6a22"
+  apiKey: "AIzaSyBpnyAOhGRyzO7o20nBhoVSnsEFg11cUFY",
+  authDomain: "visualiser-83148.firebaseapp.com",
+  projectId: "visualiser-83148",
+  storageBucket: "visualiser-83148.appspot.com",
+  messagingSenderId: "216546935300",
+  appId: "1:216546935300:web:b2c36d405023301de02b51"
 });
 
 export const db = app.firestore();
